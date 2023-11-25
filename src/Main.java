4513dc85c2b53cc1d9e364602940ab20434a3895
Davidcl-lp <dclworker@gmail.com>
2023-11-25 20:04:52 +0000

    Code completed

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,7 @@
+import java.time.LocalDate;
 public class Main {
+    public static void main(String[] args){
+        Person person = new Person("Example", LocalDate.of(1996, 5, 1));
+        System.out.println(person.toString());
+    }
 }
